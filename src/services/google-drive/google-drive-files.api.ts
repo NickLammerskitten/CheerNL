c6c1c5daa3beb50843bb0478a5638b43708f1cba
NickLammerskitten <120336345+NickLammerskitten@users.noreply.github.com>
2025-12-30 21:32:24 +0100

    Remove unused google drive functions and move folder ids to env variables

--- src/services/google-drive/google-drive-files.api.ts
+++ src/services/google-drive/google-drive-files.api.ts
@@ -4,13 +4,8 @@ import { createGoogleDriveClient } from "@/utils/google/google-drive";
 import { drive_v3 } from 'googleapis';
 import { GaxiosResponseWithHTTP2 } from "googleapis-common";
 
-// TODO: Save in Database
-const TRAININGSPLAN_FOLDER_ID = '1pyWu9XFLaOVbH97BUr28ZHpkRj39EM7r';
-const TRAININGSPLAN_VORLAGEN_FOLDER_ID = '1eClkcETwLIOpDkVXxDbPn5cWe4EqFocp';
-
-export const googleDriveFolderId = async (): Promise<string> => {
-    return TRAININGSPLAN_FOLDER_ID;
-}
+const TRAININGSPLAN_FOLDER_ID = process.env.TRAININGSPLAN_FOLDER_ID!;
+const TRAININGSPLAN_VORLAGEN_FOLDER_ID = process.env.TRAININGSPLAN_VORLAGEN_FOLDER_ID!;
 
 export const listTrainingsplanVorlagenFiles = async (): Promise<GaxiosResponseWithHTTP2<drive_v3.Schema$FileList>> => {
     const drive = await createGoogleDriveClient();
@@ -22,17 +17,6 @@ export const listTrainingsplanVorlagenFiles = async (): Promise<GaxiosResponseWi
         supportsAllDrives: true,
     });
 }
-
-export const listAll = async (): Promise<GaxiosResponseWithHTTP2<drive_v3.Schema$FileList>> => {
-    const drive = await createGoogleDriveClient();
-
-    const searchString = `'${TRAININGSPLAN_FOLDER_ID}' in parents`
-
-    return await drive.files.list({
-        q: searchString,
-        supportsAllDrives: true,
-    });
-};
 
 export const getFile = async (fileId: string): Promise<GaxiosResponseWithHTTP2<drive_v3.Schema$File>> => {
     const drive = await createGoogleDriveClient();
